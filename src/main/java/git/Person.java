ab3240cf8d82182de33e7b4637634ce597a69e0a
Crosh1t <loookintome@gmail.com>
2025-06-02 23:23:10 +0900

    Вливание веток

--- src/main/java/git/Person.java
+++ src/main/java/git/Person.java
@@ -2,6 +2,7 @@ package git;
 
 public class Person {
     private String name;
+    private int age;
 
     public String getName() {
         return name;
@@ -9,5 +10,13 @@ public class Person {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
     }
 }
