49a4bd04126da5c2682ef7224b6d6d85d27fe0f1
Crosh1t <loookintome@gmail.com>
2025-08-13 02:04:21 +0900

    Литкод таски

--- src/main/java/Leetcode/Algoritmic/LongestCommonPrefix/Solution.java
+++ src/main/java/Leetcode/Algoritmic/LongestCommonPrefix/Solution.java
@@ -4,96 +4,39 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Solution {
-    //Пробую решение в лоб
-    //чисто на поиск в строке соответствия
-//    public static String longestCommonPrefix(String[] strs) {
-//        StringBuilder sb = new StringBuilder();
-//        //#Проверяем самый короткий элемент, чтобы знать максимальное совпадение по длине символов
-//        int lowSizeElement = 201;
-//        for (int i = 0; i < strs.length; i++) {
-//            if (lowSizeElement > strs[i].length()) {
-//                lowSizeElement = strs[i].length();
-//            }
-//        }
-//        //# конец блока
-//
-//        //Далее начало вложенности и проверка совпадений по строкам
-//        List<String> trimList = new ArrayList<>();
-//        for (int i = 0; i < strs.length; i++) {
-//            if (i == 0) sb.append(strs[i].charAt(0) + strs[i].charAt(1));
-//            if (sb.toString().contains(strs[i])) {
-//
-//            }
-//        }
-//
-//
-//        System.out.println("Самый короткий элемент: " + lowSizeElement);
-//        System.out.println("There is no common prefix among the input strings.");
-//        return null;
-//    }
-
-//    public static String longestCommonPrefix(String[] strs) {
-//        String prefix = "";
-//        int lowSizeElement = 201;
-//        for (int i = 0; i < strs.length; i++) {
-//            if (lowSizeElement > strs[i].length()) {
-//                lowSizeElement = strs[i].length();
-//            }
-//            if (strs.length == 1) return prefix = strs[0];
-//        }
-//        if (lowSizeElement < 1) {return prefix;}
-//
-//        System.out.println("Самый короткий элемент " + lowSizeElement);
-//        int i = 0;
-//        if (1 >= strs.length || strs[i].length() <= 1) return strs[i];
-//        while (i < strs.length) {
-//            if (prefix.length() < 2 && strs[i].length() >= i) {
-//                prefix = prefix + strs[i].charAt(i);
-//            }
-//            if (!strs[i].contains(prefix)) {
-//                return "";
-//            }
-//            i++;
-//        }
-//        return prefix;
-//    }
-
     public static String longestCommonPrefix(String[] strs) {
-        String prefix = "";
-        int lowSizeElement = 201;
+        int minlenghtUnderString = strs[0].length();
+        List<String> subStrings = new ArrayList<>();
+        //нашли минимальную строку из массива строк
         for (int i = 0; i < strs.length; i++) {
-            if (lowSizeElement > strs[i].length()) {
-                lowSizeElement = strs[i].length();
-                prefix = strs[i];
+            if (strs[i].length() < minlenghtUnderString) {
+                minlenghtUnderString = strs[i].length();
             }
-            if (strs.length == 1) return prefix = strs[0];
+        }
+        //щас будем искать все подстроки в массиве
+        for (int i = 0; i < strs.length; i++) {
+            for (int j = 0; j < strs[i].length(); j++) {
+                subStrings.add(strs.)
+            }
         }
 
+        String result = "";
+        for (int i = 0; i < strs.length; i++) {
+            for (int j = 0; j < strs[i].length(); j++) {
+                if (i == 0 && j == 0) {result = strs[i].charAt(j) + "";}
 
-        System.out.println("Самый короткий элемент " + lowSizeElement);
-        int i = 0;
-
-        while (i < strs.length) {
-            if (!strs[i].contains(prefix)) {
-                return "";
+               // if ()
             }
-            i++;
         }
-        return prefix;
+        return result;
     }
-
     public static void main(String[] args) {
-        String[] strs = new String[] {"flower","flow","flight"};
-        String[] strs2 = new String[] {"dog","racecar","car"};
-        String[] strs3 = new String[] {""};
-        String[] strs4 = new String[] {"a"};
-        String[] strs5 = new String[] {"ab", "a"};
-
-        System.out.println("Результат 1: " + longestCommonPrefix(strs));
-       System.out.println("Результат 2: " + longestCommonPrefix(strs2));
-        System.out.println("Результат 3: " + longestCommonPrefix(strs3));
-        System.out.println("Результат 4: " + longestCommonPrefix(strs4));
-        System.out.println("Результат 5: " + longestCommonPrefix(strs5));
+        System.out.println("Результат выполнения с строкой [\"flower\", \"flow\", \"flight\"] - "
+                + longestCommonPrefix(new String[] {"flower", "flow", "flight"}) );
+        System.out.println("Результат выполнения с строкой [\"dog\",\"racecar\",\"car\"]  - "
+                + longestCommonPrefix(new String[] {"dog","racecar","car"}) );
+        System.out.println("Результат выполнения с строкой [\"a\",\"aa\"]  - "
+                + longestCommonPrefix(new String[] {"a","aa"}) );
 
     }
 }
--- src/main/java/Leetcode/Algoritmic/RomanToInteger/Solution.java
+++ src/main/java/Leetcode/Algoritmic/RomanToInteger/Solution.java
@@ -25,15 +25,108 @@ public class Solution {
 //        return result;
 //    }
 
+//    public static int romanToInt(String s) {
+//        //s = new StringBuilder(s).reverse().toString();
+//        int result = 0;
+//        int minVal = 0;
+//        Map<Character, Integer> romMap = new HashMap<>();
+//        romMap.put('I', 1);
+//        romMap.put('V', 5);
+//        romMap.put('X', 10);
+//        romMap.put('L', 50);
+//        romMap.put('C', 100);
+//        romMap.put('D', 500);
+//        romMap.put('M', 1000);
+//
+//        for (int i = 0; i < s.length(); i++) {
+//            if (minVal > romMap.get(s.charAt(i))) {
+//                result -= romMap.get(s.charAt(i));
+//                minVal = romMap.get(s.charAt(i));
+//            }
+//            result += romMap.get(s.charAt(i));
+//        }
+//        System.out.println(s + " " + result);
+//        return result;
+//    }
+
+//    public static int romanToInt(String s) {
+//        StringBuilder sb = new StringBuilder(s);
+//        int result = 0;
+//        int pointer = 0;
+//        char valprev = sb.charAt(0);
+//
+//        Map<Character, Integer> romMap = new HashMap<>();
+//        romMap.put('I', 1);
+//        romMap.put('V', 5);
+//        romMap.put('X', 10);
+//        romMap.put('L', 50);
+//        romMap.put('C', 100);
+//        romMap.put('D', 500);
+//        romMap.put('M', 1000);
+//        for (int i = 0; i < sb.length(); i++) {
+//            if (pointer == 0) {
+//                pointer += romMap.get(sb.charAt(i));
+//                continue;
+//            }
+//            if (pointer < romMap.get(sb.charAt(i))) {
+//                result += romMap.get(sb.charAt(i)) - pointer;
+//                pointer = 0;
+//
+//            }
+//            if (pointer == romMap.get(sb.charAt(i)) | (pointer / 2 == romMap.get(sb.charAt(i)))) {
+//                pointer += romMap.get(sb.charAt(i));
+//
+//            }
+//            if (pointer > romMap.get(sb.charAt(i))){
+//                result += pointer;
+//                pointer = 0;
+//            }
+//
+//        }
+//        System.out.println(result);
+//        return result;
+//    }
+
     public static int romanToInt(String s) {
+        StringBuilder sb = new StringBuilder(s);
         int result = 0;
+        int potential = 0;
         Map<Character, Integer> romMap = new HashMap<>();
-        System.out.println(s + " " + result);
+        romMap.put('I', 1);
+        romMap.put('V', 5);
+        romMap.put('X', 10);
+        romMap.put('L', 50);
+        romMap.put('C', 100);
+        romMap.put('D', 500);
+        romMap.put('M', 1000);
+
+        for (int i = 0; i < sb.length(); i++) {
+            result += romMap.get(sb.charAt(i));
+            if (potential == 0) {
+                potential = romMap.get(sb.charAt(i));
+            }
+            if (potential > romMap.get(sb.charAt(i))) {
+                potential =  romMap.get(sb.charAt(i));
+            }
+            if (potential / 2 == romMap.get(sb.charAt(i)) ) {
+                potential += romMap.get(sb.charAt(i));
+
+            }
+            if (potential ==  romMap.get(sb.charAt(i))) {
+                potential +=  romMap.get(sb.charAt(i));
+            }
+            if (potential < romMap.get(sb.charAt(i))) {
+                result = result - potential;
+                potential = 0;
+            }
+
+        }
         return result;
     }
     public static void main(String[] args) {
-        romanToInt("III");
-        romanToInt("LVIII");
-        romanToInt("MCMXCIV");
+        romanToInt("LIIIV");
+        romanToInt("LVIII");  // 58
+        romanToInt("MCMXCIV"); // 1994
+        //romanToInt("XIII");
     }
 }
